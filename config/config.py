811fa6bec94f1bc5c59ee092ede8bdb6e0f36f3d
apihashteam <105586053+apihashteam@users.noreply.github.com>
2022-05-25 19:48:06 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/anonymousteambot/YukkiMusicBot",
+    "https://github.com/apihashteam/YukkiMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
